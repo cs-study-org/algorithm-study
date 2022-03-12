11054012f373999a7fcf2933bf275fd2fce483a3
Brave Kim <ygflove@bible.ac.kr>
2022-03-12 19:50:02 +0900

    :zap: Improve time complexity at Lv3.베스트앨범

--- 06/yongki/src/Lv3.bestAlbum.js
+++ 06/yongki/src/Lv3.bestAlbum.js
@@ -12,17 +12,51 @@ MyHashMap.prototype._getHash = function (str) {
   return hash;
 }
 
-function clusterBygenre(hashMap) {
-  return Object.keys(hashMap.table).map(key => {
+function clusterPlayByGenre(hashMap, genres) {
+  const cluster = [];
+
+  genres.forEach(genre => {
     const result = [];
-    let cur = hashMap.table[key].head;
+
+    const hash = hashMap._getHash(genre);
+    let cur = hashMap.table[hash].head;
 
     while (cur) {
-      result.push(cur.value)
+      result.push(cur.value.play)
       cur = cur.next;
     }
-    return result;
-  });
+
+    cluster.push({ 'name': genre, 'plays': result });
+  })
+
+  return cluster;
+}
+
+function sortPlayByCount(a, b) {
+  return Math.max(...b.plays) - Math.max(...a.plays);
+}
+
+function sortPlayByCountAtGenre(a, b) {
+  return b - a;
+}
+
+function slicePlayTop2AtGenre(plays) {
+  return plays.slice(0, 2)
+}
+
+function findOrdinaryIdxAtPlay(hashMap, genre, play) {
+  const hash = hashMap._getHash(genre);
+
+  let cur = hashMap.table[hash].head;
+
+  while (cur) {
+    if (cur.value.play === play)
+      return cur.value.ordinaryIdx;
+
+    cur = cur.next;
+  }
+
+  return;
 }
 
 /** 
@@ -31,35 +65,42 @@ function clusterBygenre(hashMap) {
  * @return {Array[number]}
  * 
  * a as genres | plays
- * b as nested array (clusterBygenre)
- * c as concat array (sortedPlays)
+ * b as set genre         (※ b is less than a)
+ * c as clustered play    (※ c is less than a)
  * 
- * time:    O(ac + b)
+ * time:    O(bc + a)
  *          for               → O(a)
- *          clusterBygenre    → O(a)
- *          map               → O(b)
- *            sort              → O(a log a)
- *          sort              → O(b)
  * 
- *          map               → O(c)
- *            indexOf           → O(a)
+ *          clusterBygenre    → O(b)
+ *            forEach           → O(c)
+ * 
+ *          forEach           → O(b)
+ *            sort              → O(c log c)
+ * 
+ *          map               → O(b)
+ *            map               → O(c)
  * 
  * space:   O(a)
  */
 function solution(genres, plays) {
-  const hashMap = new MyHashMap();  
+  const hashMap = new MyHashMap();
 
   for (let i = 0; i < genres.length; i++)
-    hashMap.put(genres[i], plays[i]);    
+    hashMap.put(genres[i], { 'ordinaryIdx': i, 'play': plays[i] });
 
-  const sortedClusterBygenre = clusterBygenre(hashMap)
-    .map(cluster => cluster.sort((a, b) => b - a))
-    .sort((a, b) => Math.max(...b) - Math.max(...a))
-    .map(cluster => cluster.slice(0, 2));  
+  const setGenres = new Set(genres);
 
-  const sortedPlays = [].concat(...sortedClusterBygenre);  
+  const sortedGenres = clusterPlayByGenre(hashMap, setGenres)
+    .sort(sortPlayByCount);
 
-  return sortedPlays.map(play => plays.indexOf(play));
+  sortedGenres.forEach(each => each.plays.sort(sortPlayByCountAtGenre))
+  sortedGenres.forEach(each => each.plays = slicePlayTop2AtGenre(each.plays));  
+  
+  return [].concat(...sortedGenres.map(genre => {
+    return genre.plays.map(play => {
+      return findOrdinaryIdxAtPlay(hashMap, genre.name, play)
+    })
+  }));
 }
 
 (function main() {
