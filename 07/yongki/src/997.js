198866a5f038a049af79778bd78c2413e2b06328
Brave Kim <ygflove@bible.ac.kr>
2022-03-29 10:27:10 +0900

    :recycle: Refactor 997
    adjacencyMatrix to adjacencyList

--- 07/yongki/src/997.js
+++ 07/yongki/src/997.js
@@ -1,23 +1,30 @@
 const assert = require('assert');
 const util = require('util');
 
-const AdjacencyMatrix = require('../../../ADT/yongki/Graph/AdjacencyMatrix');
+const AdjacencyList = require('../../../ADT/yongki/Graph/AdjacencyList');
 
-var isJudgeExist = function (trustCounter) {
-  return !(
-    Object.values(trustCounter).every(each => each === 1)
-    && Object.values(trustCounter).length > 1
-  );
+var makeTrustCounter = function (trust) {
+  const trustCounter = {};
+
+  for (const edge of trust) {
+    const trustOne = edge[1];
+    trustCounter[trustOne] = trustCounter[trustOne]
+      ? trustCounter[trustOne] + 1
+      : 1;
+  }
+
+  return trustCounter;
 }
 
-var isJudgeTrustOne = function (trust, graph) {
-  for (const edge of trust) {
-    const [vertexA, vertexB] = edge;
-    const isDirected = graph.matrix.get(vertexA)[vertexB];
-    const isUndirected = graph.matrix.get(vertexB)[vertexA];
+var isTrustEachother = function (trustCounter) {
+  return Object.values(trustCounter).every(each => each === 1)
+    && Object.keys(trustCounter).length > 1;
+}
 
-    if (isDirected && isUndirected)
-      return true;
+var isJudgeExist = function (list) {
+  for (let vertex = 1; vertex < list.length; vertex++) {
+    if (!list[vertex].size)
+      return true
   }
 
   return false;
@@ -37,7 +44,7 @@ var findJudge = function (n, trust) {
   if (!trust.length)
     return vertexs;
 
-  const graph = new AdjacencyMatrix();
+  const graph = new AdjacencyList();
 
   for (let vertex = 1; vertex <= vertexs; vertex++)
     graph.insertVertex(vertex);
@@ -47,22 +54,29 @@ var findJudge = function (n, trust) {
     graph.insertEdge(vertexA, vertexB);
   }
 
-  if (isJudgeTrustOne(trust, graph))
+  // console.log(util.inspect(graph, { showHidden: false, depth: null }));
+
+  const trustCounter = makeTrustCounter(trust);
+
+  if (!isJudgeExist(graph.list))
     return -1;
 
-  let trustCounter = {};
-
-  for (const edge of trust) {
-    const trustOne = edge[1];
-    trustCounter[trustOne] = trustCounter[trustOne]
-      ? trustCounter[trustOne] + 1
-      : 1;
-  }
-
-  if (!isJudgeExist(trustCounter))
+  if (isTrustEachother(trustCounter))
     return -1;
 
-  return Object.keys(trustCounter)[0];
+  console.log(util.inspect(trustCounter, { showHidden: false, depth: null }));
+
+  const maxTrustOne = Number(Object.keys(trustCounter)[0]);
+  const maxTrustCount = Object.values(trustCounter)[0];
+  const maxTrustWithoutJudge = vertexs - 1;
+
+  if (
+    (maxTrustWithoutJudge == maxTrustCount && trust.length === 1)
+    || maxTrustWithoutJudge == maxTrustCount
+  )
+    return maxTrustOne;
+
+  return -1;
 };
 
 (function main() {
@@ -89,5 +103,10 @@ var findJudge = function (n, trust) {
   assert.equal(
     findJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]),
     3
+  );
+
+  assert.equal(
+    findJudge(4, [[1, 3], [1, 4], [2, 3]]),
+    -1
   );
 })();
